c69175428af9f12aa2800c30bd6306db19b90ee9
SamLangton <samantha.langton@kurtosys.com>
2018-02-07 14:01:45 +0200

    Edited Code Breaking challenge

--- CodeBreaking.js
+++ CodeBreaking.js
@@ -2,9 +2,10 @@ function compress(string)
 {
     var count = 0;
     var comp = "";
-    var letter = string.CharAt(0);
+    var letter = string.charAt(0);
     for(x of string)
     {
+        console.log(x);
         if(x == letter)
         {
             count++;
@@ -12,8 +13,38 @@ function compress(string)
         else{
             comp = comp + letter + count.toString();
             letter = x;
+            console.log("Now using letter" + letter);
+            count = 1;
         }
     }
+    comp = comp + letter + count.toString();
+    console.log(comp);
 }
-var string = "aabbbcccccaa";
-compress(string);
+
+function decompress(string){
+    var decomp = "";
+    var counter = 1;
+    for (x of string){
+        if(counter % 2 != 0){
+            var letter = x;
+            counter++;
+        }
+        else{
+            count = parseInt(x);
+            var numcount = "";
+            for (var i = 0; i < count; i++)
+            {
+                numcount = numcount + letter;
+            }
+            decomp = decomp + numcount;
+            counter++;
+        }
+    }
+    console.log(decomp);
+}
+
+
+
+
+var string = "a2b3c5a2";
+decompress(string);
